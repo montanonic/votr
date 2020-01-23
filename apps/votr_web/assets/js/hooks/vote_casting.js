18ec99ae10bd474097f5ce62da9346510191fc19
Nicholas Montaño <montanonic@gmail.com>
2020-01-22 21:59:39 -0800

    Sync vote casting with server data

--- apps/votr_web/assets/js/hooks/vote_casting.js
+++ apps/votr_web/assets/js/hooks/vote_casting.js
@@ -1,16 +1,46 @@
 import Sortable from "sortablejs"
 
+export const voteCasting = {
+  mounted() {
+    const pushEventToSelf = (event, payload) =>
+      this.pushEventTo(this.selector, event, payload)
+
+    // Make the options list sortable.
+    const optionsList = document.getElementById("vote-options-list")
+    createSortable(optionsList, "options-list", pushEventToSelf)
+
+    // Make each rank group sortable.
+    const numOptions = optionsList.childElementCount
+    for (let rankGroup of getRankGroups(numOptions)) {
+      createSortable(rankGroup, rankGroup.dataset.id, pushEventToSelf)
+    }
+
+    // addSubmitVotesListener(numOptions, pushEventToSelf)
+  },
+  selector: "#vote-casting-component"
+}
+
+function getRankGroups(numOptions) {
+  const rankGroups = []
+  for (let i = 1; i <= numOptions; i++) {
+    const groupName = `group-${i}`
+    const rankGroup = document.getElementById(groupName)
+    rankGroups.push(rankGroup)
+  }
+  return rankGroups
+}
+
 function createSortable(elem, groupName, pushEventToSelf) {
   return new Sortable(elem, {
-    // No server-side handling until we send the full voting results.
-    // onAdd(e) {
-    //   const { from, to, item } = e
-    //   pushEventToSelf("move_vote", {
-    //     from: from.dataset.id,
-    //     to: to.dataset.id,
-    //     voteOptionId: item.dataset.optionId
-    //   })
-    // },
+    onAdd(e) {
+      const { from, to, item, newDraggableIndex } = e
+      pushEventToSelf("move_vote", {
+        from: from.dataset.id,
+        to: to.dataset.id,
+        new_index: newDraggableIndex,
+        option_id: Number(item.dataset.id)
+      })
+    },
     sort: false,
     animation: 150,
     pull: true,
@@ -20,23 +50,4 @@ function createSortable(elem, groupName, pushEventToSelf) {
     dragClass: "sortable-drag", // Class name for the dragging item
     group: { name: groupName, put: true }
   })
-}
-
-export const voteCasting = {
-  mounted() {
-    const pushEventToSelf = (event, payload) =>
-      this.pushEventTo(this.selector, event, payload)
-
-    const optionsList = document.getElementById("vote-options-list")
-    createSortable(optionsList, "options-list", pushEventToSelf)
-
-    const numOptions = optionsList.childElementCount
-
-    for (let i = 1; i <= numOptions; i++) {
-      const groupName = `group-${i}`
-      const rankGroup = document.getElementById(groupName)
-      createSortable(rankGroup, groupName, pushEventToSelf)
-    }
-  },
-  selector: "#vote-casting-component"
 }
--- apps/votr_web/lib/votr_web/live/vote/vote_casting.ex
+++ apps/votr_web/lib/votr_web/live/vote/vote_casting.ex
@@ -6,6 +6,10 @@ defmodule VotrWeb.VoteLive.VoteCasting do
     VotrWeb.VoteView.render("vote_casting.html", assigns)
   end
 
+  def mount(socket) do
+    {:ok, assign(socket, validating?: false)}
+  end
+
   def update(assigns, socket) do
     m(%{vote_options, room}) = assigns
 
@@ -28,50 +32,34 @@ defmodule VotrWeb.VoteLive.VoteCasting do
     {:noreply, socket}
   end
 
-  # def handle_event(
-  #       "move_vote",
-  #       %{"from" => from, "to" => to, "voteOptionId" => option_id},
-  #       socket
-  #     ) do
-  #   option_id = String.to_integer(option_id)
-  #   from = parse_location(from)
-  #   to = parse_location(to)
+  def handle_event("confirm_submission", _params, socket) do
+    {:noreply, socket}
+  end
 
-  #   option =
-  #     socket.assigns.vote_options
-  #     |> Enum.find(fn vo -> vo.id == option_id end)
+  def handle_event("edit_submission", _params, %{assigns: m(%{validating?})} = socket)
+      when validating? do
+    {:noreply, assign(socket, validating?: false)}
+  end
 
-  #   options_by_rank =
-  #     socket.assigns.options_by_rank
-  #     |> Map.update!(from, &Enum.reject(&1, fn vo -> vo.id == option_id end))
-  #     |> Map.update!(to, &(&1 ++ [option]))
+  def handle_event("submit_votes", _params, socket) do
+    IO.inspect(socket.assigns.options_by_rank)
+    {:noreply, assign(socket, validating?: true)}
+  end
 
-  #   {:noreply, assign(socket, options_by_rank: options_by_rank)}
-  # # end
+  def handle_event("move_vote", params, socket) do
+    ms(%{from, to, new_index, option_id}) = params
+    m(%{vote_options, options_by_rank}) = socket.assigns
 
-  # def handle_event(up_or_down, %{"id" => id}, socket) when up_or_down in ~w|up down| do
-  #   id = String.to_integer(id)
+    vote_option = Enum.find(vote_options, &(&1.id == option_id))
 
-  #   update_vote_option = fn vos ->
-  #     import Access
-  #     len = length(vos)
+    options_by_rank =
+      options_by_rank
+      |> Map.update!(parse_rank(from), &Enum.reject(&1, fn m(%{id}) -> id == option_id end))
+      |> Map.update!(parse_rank(to), &List.insert_at(&1, new_index, vote_option))
 
-  #     op =
-  #       case up_or_down do
-  #         "up" -> &max(&1 - 1, 1)
-  #         "down" -> &min(&1 + 1, len)
-  #       end
+    {:noreply, assign(socket, mk(%{options_by_rank}))}
+  end
 
-  #     i = Enum.find_index(vos, &(&1.id == id))
-
-  #     update_in(vos, [at(i), key(:rank)], op)
-  #   end
-
-  #   room = Map.update!(socket.assigns.room, :vote_options, update_vote_option)
-
-  #   {:noreply, assign(socket, room: room)}
-  # end
-
-  # defp parse_location("menu"), do: :menu
-  # defp parse_location(str), do: String.to_integer(str)
+  defp parse_rank("menu"), do: :menu
+  defp parse_rank(str), do: String.to_integer(str)
 end
